24562a1ff90a6e8e109b1dd699f66b9dd2a45159
My Name <darkarchorn@gmail.com>
2022-03-23 17:34:38 +0700

    Add code.cpp file

---
+++ code.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int scs(int c)
+{
+    if(c==0) return 1;
+    int a = c;
+	int dem = 0;
+	while(a){
+        a=a/10;
+        dem++;
+	}
+	return dem;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	int a[n], b[n];
+	for(int i=0;i<n;i++) {
+		cin >> a[i] >> b[i];
+	}
+
+	for(int i=0;i<n;i++) {
+        if(a[i]==0 && b[i]==0) {
+            cout << 0 <<endl;
+            break;
+        }
+        else if(a[i]==0 && b[i]!=0) {
+            cout << -1 <<endl;
+            break;
+        }
+	    int dem = scs(a[i]);
+		while(a[i]) {
+			if(a[i] % 10 == b[i]) {
+				cout << dem-1 << endl;
+				break;
+			}
+			else a[i] = a[i]/10;
+            dem--;
+            if(dem==0) cout << -1 <<endl;
+		}
+	}
+}
